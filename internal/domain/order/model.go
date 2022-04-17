4d0b5af493cd7b7446eaa7accbba3acccd0be377
Mert Şakar <mertsakar@hotmail.com>
2022-04-18 01:56:54 +0300

    stock changes after create order and cancel order handled

--- internal/domain/order/model.go
+++ internal/domain/order/model.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/horzu/golang/cart-api/internal/domain/order/orderItem"
+	"github.com/horzu/golang/cart-api/internal/domain/product"
 	"gorm.io/gorm"
 )
 
@@ -45,4 +46,32 @@ func NewOrderItem(quantity uint, productID string) *orderItem.OrderItem {
 		Quantity:   quantity,
 		ProductId:  productID,
 	}
+}
+
+// BeforeUpdate checks if an order is canceled. If the order is canceled stock updates.
+func (order *Order) BeforeUpdate(tx *gorm.DB) (err error) {
+
+	if order.IsCanceled {
+		var orderedItems []orderItem.OrderItem
+		if err := tx.Where("order_id = ?", order.Id).Find(&orderedItems).Error; err != nil {
+			return err
+		}
+		for _, item := range orderedItems {
+			var orderedItemsProducts product.Product
+			if err := tx.Where("ID = ?", item.ProductId).First(&orderedItemsProducts).Error; err != nil {
+				return err
+			}
+			updatedStock := orderedItemsProducts.Stock + int64(item.Quantity)
+			if err := tx.Model(&orderedItemsProducts).Update(
+				"stock", updatedStock).Error; err != nil {
+				return err
+			}
+			if err := tx.Model(&item).Update(
+				"is_canceled", true).Error; err != nil {
+				return err
+			}
+		}
+	}
+	return
+
 }
--- internal/domain/order/orderItem/model.go
+++ internal/domain/order/orderItem/model.go
@@ -1,6 +1,7 @@
 package orderItem
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -33,4 +34,30 @@ func NewOrderItem(cartItem cartItem.CartItem) *OrderItem {
 		Quantity:  cartItem.Quantity,
 	}
 	return item
+}
+
+// BeforeSave updates products' stock count after completing order
+func (orderedItem *OrderItem) BeforeSave(tx *gorm.DB) (err error) {
+
+	var currentProduct product.Product
+	var currentOrderedItem OrderItem
+	if err := tx.Where("ID = ?", orderedItem.ProductId).First(&currentProduct).Error; err != nil {
+		return err
+	}
+	reservedStockCount := 0
+	if err := tx.Where("ID = ?", orderedItem.Id).First(&currentOrderedItem).Error; err == nil {
+		reservedStockCount = int(currentOrderedItem.Quantity)
+	}
+	newStockCount := int(currentProduct.Stock) + reservedStockCount - int(orderedItem.Quantity)
+	if newStockCount < 0 {
+		return errors.New("Not Enough Stock")
+	}
+	if err := tx.Model(&currentProduct).Update("stock", newStockCount).Error; err != nil {
+		return err
+	}
+	if orderedItem.Quantity == 0 {
+		err := tx.Unscoped().Delete(currentOrderedItem).Error
+		return err
+	}
+	return
 }
